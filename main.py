db17597bf4460f0ea2c3c16b067b3640c2391a0d
oslook <oslook@users.noreply.github.com>
2021-09-07 23:11:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -185,7 +185,7 @@ def readme(stars):
         f.write("## Want to contribute?\n\nClicking the star will trigger the commit which includes the clicker's name to the contributors list. So CLICK THE STAR!")
     with open("/tmp/user.txt", 'w') as f:
         if stars:
-            f.write("%s %s %d" % (stars[0]['login'], stars[0]['email'], len(stars)))
+            f.write("%s %d" % (stars[0]['login'], len(stars)))
     
 if __name__ == "__main__":
     # args > 1 for processing
